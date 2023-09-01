5a243309efa83de0c34b0ab1da61e6e1706cb716
bygimenezz <bygimenezz@gmail.com>
2023-09-01 12:27:25 +0200

    fix login netlify

--- components/auth-button-client.tsx
+++ components/auth-button-client.tsx
@@ -15,7 +15,7 @@ export function AuthButton({ session }: { session: Session | null }) {
     await supabase.auth.signInWithOAuth({
       provider: "discord",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: "https://model-ai.vercel.app/auth/callback",
       },
     });
   };
